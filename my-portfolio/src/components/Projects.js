ac993c4061d895b63bd995c60bb04c5bd7701f27
Th0masSh3lby <99462129+Th0masSh3lby@users.noreply.github.com>
2023-06-14 15:05:19 +0530

    linked projects page using react router dom

---
+++ my-portfolio/src/components/Projects.js
@@ -0,0 +1,76 @@
+import rqm from "./RQM.PNG";
+import drum from "./DrumMachine.PNG";
+import fccmag from "./fccMag.PNG";
+import clock from "./Clock.PNG";
+import "./projects.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
+const allProjects = [
+  {
+    name: "Random-Quote",
+    description: `Random quote machine is a web application that generates a random quote from a collection of quotes. It is built using React.js, HTML, and CSS. The quotes were fetched randomly using an API.`,
+    backgroundUrl: rqm,
+    WebsiteLink: "https://th0massh3lby.github.io/Random-Quote-Machine/",
+    GitLink: "https://github.com/Th0masSh3lby/Random-Quote-Machine",
+  },
+
+  {
+    name: "Drum Machine",
+    description: `
+    This website is a drum machine that can be used to create and play custom beats. It was created using React.js, HTML, and CSS. The website is simple to use and allows users to create beats by clicking on the different drum pads. `,
+    backgroundUrl: drum,
+    WebsiteLink: "https://th0massh3lby.github.io/drum-machine/",
+    GitLink: "https://github.com/Th0masSh3lby/drum-machine",
+  },
+  {
+    name: "FCC Magazine",
+    description: `
+    This website is a single page application done as a part of my Responsive Web Design Certification. Created using HTML and CSS. It was made such that the page responds responsively to the screensize. `,
+    backgroundUrl: fccmag,
+    WebsiteLink: "https://th0massh3lby.github.io/FreeCodeCamp-magazine/",
+    GitLink: "https://github.com/Th0masSh3lby/FreeCodeCamp-magazine",
+  },
+  {
+    name: "Pomodoro Clock",
+    description: `This website is an easy to use pomodoro clock.  The user can choose the length of the pomodoro intervals and the length of the breaks. It was created using various React hooks, CSS and HTML.`,
+    backgroundUrl: clock,
+    WebsiteLink: "https://th0massh3lby.github.io/Clock-25-5/",
+    GitLink: "https://github.com/Th0masSh3lby/Clock-25-5",
+  },
+];
+
+const Projects = () => {
+  return (
+    <div className="Mains">
+      <div className="actual">
+        <h1 id="pname">All Projects</h1>
+        {allProjects.map((obj) => {
+          return (
+            <div className="ProjectS">
+              <a target="_blank" href={obj.WebsiteLink}>
+                <img className="pr" src={obj.backgroundUrl} />
+              </a>
+
+              <div className="description">
+                <h3>{obj.name}</h3>
+                <p>{obj.description}</p>
+                <div className="allGithub">
+                  <a
+                    href={obj.GitLink}
+                    target="_blank"
+                    className="allprojectLink"
+                  >
+                    <FontAwesomeIcon className="Projecticon" icon={faGithub} />
+                  </a>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
